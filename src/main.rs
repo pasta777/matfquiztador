252dabedfc156449b269ec6cd911696fa97fdef9
pasta777 <axegamerssrb@gmail.com>
2024-04-11 00:20:58 +0200

    Added css

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk::{glib, Application, ApplicationWindow, Button, CssProvider};
+use gtk::gdk::Display;
 
 const APP_ID: &str = "rs.ac.bg.matf.matfquiztador";
 
 fn main() -> glib::ExitCode {
-    // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
-    // Connect to "activate" signal of `app`
+    app.connect_startup(|_| load_css());
     app.connect_activate(build_ui);
 
-    // Run the application
     app.run()
 }
+fn load_css() {
+    // ovo sam samo iskopirao nmp u sustini sta radi tacno ALI treba jos da implementiram
+    // strukturu fajlova i ovo je sve za sad neka vrsta placeholdera :)
+    let provider = CssProvider::new();
+    provider.load_from_path("style.css");
 
+    // Add the provider to the default screen
+    gtk::style_context_add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+
+}
 fn build_ui(app: &Application) {
-    // Create a window and set the title
 
     let button = Button::builder()
         .label("placeholder")
@@ -27,6 +38,5 @@ fn build_ui(app: &Application) {
         .child(&button)
         .build();
 
-    // Present window
     window.present();
 }
