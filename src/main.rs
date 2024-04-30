0923d2513f77457b34a0653016fb1c396be245f1
pasta777 <axegamerssrb@gmail.com>
2024-04-30 02:13:22 +0200

    Main menu overhaul + fullscreen

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 mod map;
 
 use eframe::egui;
-use egui::{Pos2};
+use egui::{Align, Layout, Pos2};
 use crate::map::SerbiaMap;
 
-// graf nije perfektan iako je skoro implementiran treba:
-// uciniti da se prozor ne resize do odredjene mere
-// urediti ui generalno
-// napraviti kolko tolku funkcionalnost - uradjeno donekle
+// na escape se izlazi sada
+
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([520.0, 340.0]),
+        viewport: egui::ViewportBuilder::default().with_fullscreen(true),
         ..Default::default()
     };
     eframe::run_native(
@@ -20,13 +18,14 @@ fn main() -> Result<(), eframe::Error> {
         Box::new(|cc| {
             // This gives us image support:
             egui_extras::install_image_loaders(&cc.egui_ctx);
-
             Box::<MyApp>::default()
         }),
     )
 }
 struct MyApp {
-    right_visible: bool,
+    show_confirmation_dialog: bool,
+    main_menu: bool,
+    playing: bool,
     serbia_map: SerbiaMap,
 }
 
@@ -34,7 +33,9 @@ impl Default for MyApp {
     // ovo je default state (kada se ucita igrica itd)
     fn default() -> Self {
         Self {
-            right_visible: true,
+            show_confirmation_dialog: false,
+            main_menu: true,
+            playing: false,
             serbia_map: SerbiaMap::new(),
         }
     }
@@ -42,31 +43,71 @@ impl Default for MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui|{
-            let panel_size = ui.available_size();
-            self.serbia_map.draw(ui, Pos2::new(panel_size.x, panel_size.y));
-        });
-        if self.right_visible {
-            egui::SidePanel::right("right_panel").show(ctx, |ui| {
-                if ui.button("Play!").clicked() {
-                    self.right_visible = false;
-                }
-                if ui.button("Settings").clicked() {
-                    //todo
-                }
+        if self.main_menu {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.style_mut().text_styles.insert(
+                    egui::TextStyle::Button,
+                    egui::FontId::new(24.0, eframe::epaint::FontFamily::Proportional)
+                );
+                ui.with_layout(Layout::top_down(Align::Center).with_cross_align(Align::Center), |ui| {
+                    // HACK ZA CENTRIRANJE TEKSTA NE KORISTITI NIGDE DRUGDE
+                    // TREBA PRONACI BOLJI NACIN ALI TOP DOWN SA CROSS ALIGN NE RADI
+                    // NE RADI NI KOMBINOVANJE HORIZONTAL CENTER I VERTICAL CENTER -- KNOWN BUG IN EGUI
+                    for _i in 1..20 {
+                        ui.label("");
+                    }
+                    // HACK SE OVDE ZAVRSAVA
+                    if ui.button("Play!").clicked() {
+                        self.main_menu = false;
+                        self.playing = true;
+                    }
+                    if ui.button("Settings").clicked() {
+                    }
+                    if ui.button("Exit").clicked() {
+                        self.show_confirmation_dialog = true;
+                    }
+                });
             });
         }
-        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
-            ui.heading("The Question placeholder");
-            ui.horizontal(|ui| {
-                let _op1 = ui.button("Option 1");
-                let _op2 = ui.button("Option 2");
+        if self.playing {
+            egui::CentralPanel::default().show(ctx, |ui|{
+                let panel_size = ui.available_size();
+                self.serbia_map.draw(ui, Pos2::new(panel_size.x, panel_size.y));
             });
-            ui.horizontal(|ui| {
-                let _op3 = ui.button("Option 3");
-                let _op4 = ui.button("Option 4");
-            });
-        });
-
+        }
+        // egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+        //     ui.heading("The Question placeholder");
+        //     ui.horizontal(|ui| {
+        //         let _op1 = ui.button("Option 1");
+        //         let _op2 = ui.button("Option 2");
+        //     });
+        //     ui.horizontal(|ui| {
+        //         let _op3 = ui.button("Option 3");
+        //         let _op4 = ui.button("Option 4");
+        //     });
+        // });
+        if self.show_confirmation_dialog {
+            egui::Window::new("Do you want to quit?")
+                .collapsible(false)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    ui.style_mut().text_styles.insert(
+                        egui::TextStyle::Button,
+                        egui::FontId::new(20.0, eframe::epaint::FontFamily::Proportional),
+                    );
+                    ui.horizontal(|ui| {
+                        if ui.button("No").clicked() {
+                            self.show_confirmation_dialog = false;
+                        }
+                        if ui.button("Yes").clicked() {
+                            self.show_confirmation_dialog = false;
+                            ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                });
+        }
+        if ctx.input(|i| i.key_pressed(egui::Key::Escape)) {
+            self.show_confirmation_dialog = true;
+        }
     }
 }
