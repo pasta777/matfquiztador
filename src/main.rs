befd8062433f36de980148eab587279b3ba2531c
DrLock02 <milos.contact.work@gmail.com>
2024-05-09 20:00:08 +0200

    WIP Question database, question logic, API injection.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod map;
 
 use eframe::egui;
 use egui::{Align, Layout, Pos2};
+use htmlentity::entity::decode;
+use rand::Rng;
+use serde::Deserialize;
 use crate::map::SerbiaMap;
 
 // na escape se izlazi sada
@@ -27,6 +30,12 @@ struct MyApp {
     main_menu: bool,
     playing: bool,
     serbia_map: SerbiaMap,
+    session_token_url: String,
+    question_vector: Vec<Question>,
+    question_generated: bool,
+    question_text: String,
+    correct_answer: String,
+    incorrect_answers: Vec<String>
 }
 
 impl Default for MyApp {
@@ -37,12 +46,18 @@ impl Default for MyApp {
             main_menu: true,
             playing: false,
             serbia_map: SerbiaMap::new(),
+            session_token_url: String::new(),
+            question_vector: Vec::new(),
+            question_generated: false,
+            question_text: String::new(),
+            correct_answer: String::new(),
+            incorrect_answers: Vec::new()
         }
     }
 }
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    async fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         if self.main_menu {
             egui::CentralPanel::default().show(ctx, |ui| {
                 ui.style_mut().text_styles.insert(
@@ -58,6 +73,13 @@ impl eframe::App for MyApp {
                     }
                     // HACK SE OVDE ZAVRSAVA
                     if ui.button("Play!").clicked() {
+                            self.session_token_url = match CreateSessionToken().await {
+                                Ok(url) => url,
+                                Err(err) => {
+                                    eprintln!("Error creating session token: {}", err);
+                                    return; // Exit the function if an error occurs
+                                }
+                            }
                         self.main_menu = false;
                         self.playing = true;
                     }
@@ -82,6 +104,15 @@ impl eframe::App for MyApp {
                 });
             }
         if self.serbia_map.show_question {
+            if self.question_vector.is_empty() {
+                self.question_vector = FillQuestionVector(self.session_token_url.clone());
+            }
+            if !self.question_generated {
+                let mut rng = rand::thread_rng();
+                let random_number: usize = rng.gen_range(0..self.question_vector.len());
+                GenerateQuestion(random_number, &mut self.question_text, &mut self.correct_answer, &mut self.incorrect_answers, &mut self.question_vector);
+                self.question_generated = true;
+            }
             egui::CentralPanel::default().show(ctx, |ui| {
                 ui.vertical_centered_justified(|ui| {
                     for _i in 1..10 {
@@ -95,18 +126,22 @@ impl eframe::App for MyApp {
                         egui::TextStyle::Heading,
                         egui::FontId::new(25.0, eframe::epaint::FontFamily::Proportional),
                     );
-                    ui.heading("The Question placeholder");
+                    ui.heading(self.question_text.clone());
                     if ui.button("Option 1").clicked() {
                         self.serbia_map.state_change(false);
+                        self.question_generated = false;
                     }
                     if ui.button("Option 2").clicked() {
                         self.serbia_map.state_change(true);
+                        self.question_generated = false;
                     }
                     if ui.button("Option 3").clicked() {
                         self.serbia_map.state_change(false);
+                        self.question_generated = false;
                     }
                     if ui.button("Option 4").clicked() {
                         self.serbia_map.state_change(false);
+                        self.question_generated = false;
                     }
                 });
             });
@@ -135,5 +170,92 @@ impl eframe::App for MyApp {
         if ctx.input(|i| i.key_pressed(egui::Key::Escape)) {
             self.show_confirmation_dialog = true;
         }
+    }
+}
+
+fn GenerateQuestion(index: usize, QuestionText: &mut String, CorrectAnswerText: &mut String,
+                    IncorrectAnswersText: &mut Vec<String>, QuestionVector: &mut Vec<Question>) {
+    let PulledQuestion = decode(QuestionVector[index].question.as_ref()).to_string();
+    let PulledCorrect = decode(QuestionVector[index].correct_answer.as_ref()).to_string();
+    let PulledIncorrects = QuestionVector[index].incorrect_answers.clone();
+    for ia in PulledIncorrects {
+        let ignusFatus = decode(ia.as_ref()).to_string();
+        match ignusFatus {
+            Ok(decoded_incorrect) => { IncorrectAnswersText.push(decoded_incorrect); },
+            Err(e) => { eprintln!("Decoding Error {}", e); }
+        }
+    }
+    match PulledQuestion {
+        Ok(decoded_question) => { *QuestionText = decoded_question; },
+        Err(e) => { eprintln!("Decoding Error {}", e); }
+    }
+    match PulledCorrect {
+        Ok(decoded_correct) => { *CorrectAnswerText = decoded_correct; },
+        Err(e) => { eprintln!("Decoding Error {}", e); }
+    }
+    QuestionVector.remove(index);
+}
+
+
+#[derive(Debug,Deserialize)]
+struct SessionToken {
+    response_code: i32,
+    response_message: String,
+    token: String
+}
+
+#[derive(Debug,Deserialize)]
+struct Question {
+    r#type: String,
+    difficulty: String,
+    category: String,
+    question: String,
+    correct_answer: String,
+    incorrect_answers: Vec<String>
+}
+
+#[derive(Debug,Deserialize)]
+struct QuestionsBatch {
+    response_code: i32,
+    results: Vec<Question>
+}
+
+#[tokio::main]
+async fn CreateSessionToken() -> Result<String, Box<dyn std::error::Error>> {
+    let mut response = reqwest::get("https://opentdb.com/api_token.php?command=request").await?;
+
+    // Check if the request was successful (status code 200)
+    if response.status().is_success() {
+        // Read the response body as text
+        let body = response.text().await?;
+        let data: SessionToken = serde_json::from_str(&body)?;
+        let session_token_real = data.token;
+
+        // Build the URL with the session token
+        let URL: &str = "https://opentdb.com/api.php?amount=50&type=multiple&token=";
+        let combined: String = format!("{}{}", URL, session_token_real);
+        Ok(combined)
+    } else {
+        println!("Request failed with status code: {}", response.status());
+        Err("Request failed".into())
+    }
+}
+
+
+#[tokio::main]
+async fn FillQuestionVector(SessionUrl: String) -> Result<Vec<Question>, Box<dyn std::error::Error>> {
+    let response = reqwest::get(&SessionUrl).await?;
+
+    if response.status().is_success() {
+        let body = response.text().await?;
+        let data: QuestionsBatch = serde_json::from_str(&body)?;
+
+        // Extract the vector of questions from the data
+        let questions_vector = data.results;
+
+        Ok(questions_vector)
+    } else {
+        println!("Request failed with status code: {}", response.status());
+        Err("Request failed".into())
     }
 }
